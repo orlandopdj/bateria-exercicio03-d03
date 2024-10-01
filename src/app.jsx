72990689542e2eb6cd4c7000c9841d1d6e811b11
orlandopdj <orlandopdjr6@gmail.com>
2024-10-01 19:39:32 -0300

    ✅ add resposta ao clicar no card

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,61 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+import { useState } from "react"
 
-export  {App}
+const cardsInfo = [
+    {
+        id: 7336,
+        question: "Do quê aplicações React são feitas?",
+        answer: "Componentes",
+    },
+    {
+        id: 8832,
+        question: "Qual é o nome da sintaxe usada para descrever UI no React?",
+        answer: "JSX",
+    },
+    {
+        id: 3457,
+        question: "Em qual linguagem o React é baseado?",
+        answer: "JavaScript",
+    },
+    {
+        id: 9103,
+        question: "Qual método usar para renderizar uma lista no React?",
+        answer: "map",
+    },
+    {
+        id: 1297,
+        question: "Qual empresa criou o React?",
+        answer: "Meta",
+    },
+    {
+        id: 2002,
+        question: "Como passar informações para um componente React?",
+        answer: "Através de props",
+    },
+]
+
+
+const App = () => {
+    const [activedId, setActivedId] = useState(0)
+    const handleLog = (id) => setActivedId((a) => (a === id ? 0 : id))
+    console.log(activedId)
+
+
+    return (
+        <ul className="flashcards">
+            {cardsInfo.map(({ id, question, answer }) => {
+                const active = id === activedId
+                return (
+                    <li
+                        key={id}
+                        className={`card ${active ? 'card-active' : ''}`}
+                        onClick={() => handleLog(id)}>
+                        {active ? answer : question}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+
+}
+
+export { App }
